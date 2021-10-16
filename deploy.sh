0c25b5628c53fff28657810a30db17b6c8b40d51
Rasmus Rendal <rasmus@rend.al>
2021-10-16 20:39:30 +0200

    Fix #7: Duplicate CTF roles

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/sh
+#!/usr/bin/env sh
 node src/deploy-commands.js
 node src/index.js 
--- src/roles.js
+++ src/roles.js
@@ -1,6 +1,14 @@
 const { teammemberrole } = require('../config.json');
 const { Permissions } = require('discord.js');
 
+// We sort of store information in Discord channel names. But they don't support
+// some characters. Spaces are also converted to dashes.
+// This function translates both a discord-mangled string and a non-mangled
+// string into the same string.
+function normalizeRoleName(rolename) {
+    return rolename.toUpperCase().replace(/ /g, '-').replace(/[']/g, "")
+}
+
 async function getTeamMemberRole(guild, ctfname) {
     if (teammemberrole === "")
         return undefined;
@@ -10,7 +18,8 @@ async function getTeamMemberRole(guild, ctfname) {
 async function getCtfRole(guild, ctfname) {
     const rolename = ctfname + " player"
     let roles = await guild.roles.fetch();
-    let ctfrole = roles.find((r) => r.name === rolename);
+    let ctfrole = roles.find((r) =>
+        normalizeRoleName(r.name) === normalizeRoleName(rolename.toUpperCase()));
     if (ctfrole !== undefined) return ctfrole;
 
     let role = await guild.roles.create({
