4c41cbd92dc2e8bedbd01729ffdc6f7d1c3e0c27
Emil Skov Olesen <emil8250@ymail.com>
2023-03-28 19:41:20 +0200

    Issue/14 enchanced categories (#32)
    * Updated readme to include Privileged Gateway Inten
    
    * Added categories to the slash command
    
    * Removed logs
    
    ---------
    
    Co-authored-by: Emil Olesen <e-olesen@hotmail.com>

--- src/commands/challenge.js
+++ src/commands/challenge.js
@@ -1,6 +1,14 @@
 const { SlashCommandBuilder, MessageMentions: { users }} = require("discord.js");
 const { getCtfNameFromChannelId, createChallenge, markChallengeAsDone, AlreadyExistsError } = require("../func.js")
-
+const Categories = {
+	Web: "web",
+	Pwn: "pwn",
+	Crypto: "crypto",
+	Forensics: "forensics",
+    Rev: "rev",
+    Misc: "misc",
+    Other: "other"
+}
 const challengeCommand = new SlashCommandBuilder()
     .setName("challenge")
     .setDescription("Add or manage challenges");
@@ -10,11 +18,32 @@ challengeCommand.addSubcommand((command) =>
     .setName("add")
     .setDescription("Add a challenge to a CTF")
     .addStringOption((option) => 
+    option
+    .setName("category")
+    .setDescription("The category of the challenge")
+    .setRequired(true)
+    .addChoices(
+        { name: 'Web', value: Categories.Web},
+        { name: 'Pwn', value: Categories.Pwn},
+        { name: 'Crypto', value:  Categories.Crypto},
+        { name: 'Forensics', value:  Categories.Forensics},
+        { name: 'Rev', value:  Categories.Rev},
+        { name: 'Misc', value:  Categories.Misc},
+        { name: 'Other', value:  Categories.Other},
+    )
+    )
+    .addStringOption((option) => 
         option
         .setName("name")
         .setDescription("The name of the challenge")
         .setRequired(true),
     )
+    .addStringOption((option) => 
+    option
+    .setName("customcategory")
+    .setDescription("Set a custom category name, only use this if 'other' is selected")
+    .setRequired(false)
+)
 );
 
 challengeCommand.addSubcommand((command) =>
@@ -33,7 +62,14 @@ module.exports = {
     async execute(interaction) {
         const guild = interaction.member.guild;
         if (interaction.options.getSubcommand() === "add") {
-            let challengeName = interaction.options.getString("name");
+            const category = interaction.options.getString("category");
+            let  challengeName = "";
+            if(category !== Categories.Other)
+                challengeName = interaction.options.getString("category");
+            else
+                challengeName = interaction.options.getString("customcategory");
+            
+            challengeName += "-" +  interaction.options.getString("name");
             let ctf = await getCtfNameFromChannelId(guild, interaction.channelId);
             if (ctf === undefined) {
                 return await interaction.reply("This command must be called from a CTF channel");
