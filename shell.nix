d337d27a85d31e85cdbe0980043a8eb4056cae6d
c3lphie <c3lphie@protonmail.com>
2024-04-25 18:03:16 +0200

    Baseline
    Det fårking virker

--- shell.nix
+++ shell.nix
@@ -1,9 +1,12 @@
-{ pkgs ? import <nixpkgs> {} }:
-
+let
+  moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
+  pkgs = import <nixpkgs> {overlays = [moz_overlay];};
+in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-	cargo
-	rustfmt
-    	clippy
+	  cargo
+	  rustfmt
+    clippy
+    latest.rustChannels.nightly.rust
   ];
 }
