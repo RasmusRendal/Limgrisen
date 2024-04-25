5b38e3d5971bfccf8b58ef6a90500b59ca1722b0
mhtoribio <23717111+mhtoribio@users.noreply.github.com>
2024-04-25 18:53:47 +0200

    add dependencies and secrets management

--- shell.nix
+++ shell.nix
@@ -1,12 +1,24 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> {overlays = [moz_overlay];};
+  discordtoken = import ./discord_token.nix;
+  guildid = import ./guild_id.nix;
 in
 pkgs.mkShell {
   buildInputs = with pkgs; [
-	  cargo
-	  rustfmt
+    cargo
+    rustfmt
     clippy
     latest.rustChannels.nightly.rust
+    pkg-config
   ];
+
+  nativeBuildInputs = with pkgs; [
+    openssl
+  ];
+
+  shellHook = ''
+      export DISCORD_TOKEN=${discordtoken}
+      export GUILD_ID=${guildid}
+  '';
 }
