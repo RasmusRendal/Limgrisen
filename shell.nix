26a83df248f226b447c882d5ddbeb03eeb0223c0
c3lphie <c3lphie@protonmail.com>
2024-04-28 15:50:31 +0200

    Ease of life additions
    - Added anyhow for result handling
    - Optimised shell.nix for better lsp integration

--- shell.nix
+++ shell.nix
@@ -1,6 +1,12 @@
 let
   moz_overlay = import (builtins.fetchTarball https://github.com/mozilla/nixpkgs-mozilla/archive/master.tar.gz);
   pkgs = import <nixpkgs> {overlays = [moz_overlay];};
+  rustChannel = pkgs.rustChannelOf {
+    channel = "stable";
+  };
+  rust = (rustChannel.rust.override {
+    extensions = ["rust-src" "rust-analysis"];
+  });
   discordtoken = import ./discord_token.nix;
   guildid = import ./guild_id.nix;
 in
@@ -9,7 +15,7 @@ pkgs.mkShell {
     cargo
     rustfmt
     clippy
-    latest.rustChannels.nightly.rust
+    rust
     pkg-config
     sqlx-cli
   ];
@@ -21,5 +27,6 @@ pkgs.mkShell {
   shellHook = ''
       export DISCORD_TOKEN=${discordtoken}
       export GUILD_ID=${guildid}
+      export DATABASE_URL=sqlite:sqlite.db
   '';
 }
